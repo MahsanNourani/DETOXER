3418ebae7e8bff1d1a8d11ae2f20f8e58c8568a3
Mahsan Nourani <mahsannourani@ufl.edu>
2020-08-04 14:01:54 -0400

    Global information works -- need to add filtering functionallity and import json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Grid, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import Mainlayout from './MainLayout';
+
+const useStyles = makeStyles ( theme => ({
+  containerStyle: {
+    backgroundColor: "#efebe9",
+    height: '100vh'
+  },
+  mainGrid: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.secondary.contrastText
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Mainlayout />
+      // <Container maxWidth="md" className={classes.containerStyle}>
+      //   <Grid container spacing={2} justify="center">
+      //     <Grid item sm={12} container justify="center" spacing={3}>
+      //       <Grid item sm={12}/>
+      //       <Grid item sm={6} className={classes.mainGrid}>
+      //         <Typography variant="h4" align="center"> Global Information</Typography>
+      //       </Grid>
+      //     </Grid>
+      //     <Grid item sm={6} container justify="center">
+      //       <Grid item sm={12} container justify="center" className={classes.mainGrid}>
+      //         <Grid item sm={8}>
+      //           <Typography variant="h5">The system accuracy is:</Typography>
+      //         </Grid>
+      //         <Grid item sm={4}>
+      //           <Typography variant="h5">85%.</Typography>
+      //         </Grid>
+      //       </Grid>
+      //     </Grid>
+      //   </Grid>
+      // </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
